f83a8ed50510cecf76116c34eeb018f290056fa0
Melida27 <mjgutierrez56@misena.edu.co>
2020-06-17 11:01:40 -0500

    ProyLaravel

--- ServiceApp/resources/views/welcome.blade.php
+++ ServiceApp/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12 text-center">
-                <h2><i class="fa fa-building"></i> Página principal ~ Reparaciones</h2>
+                <h2><i class="fa fa-building"></i> Página principal</h2>
 
                 <div class="text-center">
                 	<img src="{{asset('imgs/principal.png')}}" width="620px">
