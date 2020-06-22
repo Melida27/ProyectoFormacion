2a43f0329913b1f5e57cbb1d372af50f97593d9b
pipeosmu <pipe96-17@hotmail.com>
2020-06-22 18:41:27 -0500

    ajuste login

--- ServiceApp/public/js/login.js
+++ ServiceApp/public/js/login.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
   $('#login-button').click(function(){
     $('#login-button').fadeOut("slow",function(){
       $("#container").fadeIn();
-      TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
-      TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
     });
   });
 });
