0c0a5de594e6e5bb6ed92e66a8f805dd3296cef8
Melida27 <mjgutierrez56@misena.edu.co>
2020-07-12 13:58:34 -0500

    Alter table orders

---
+++ ServiceApp/database/migrations/2020_07_12_185014_alter_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table){
+            $table->bigInteger('fk_technical')->unsigned()->after('fk_user');
+            $table->foreign('fk_technical')->references('id')->on('users');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table){
+            $table->dropColumn(['fk_technical']);
+        });
+    }
+}
