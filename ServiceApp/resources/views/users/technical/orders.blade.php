c2e8eb6fc57a17c0c5a211649b9d7ce5271789f9
pipeosmu <pipe96-17@hotmail.com>
2020-07-13 18:44:06 -0500

    Modal de ordenes

--- ServiceApp/resources/views/users/technical/orders.blade.php
+++ ServiceApp/resources/views/users/technical/orders.blade.php
@@ -13,7 +13,7 @@
               @if($order->status_order == 'Pendiente')
                 <div class="card-order bkg_yellow">
                   <h3 class="date-order">Fecha Creacion:  {{$order->date_order}}</h3>
-                  <button type="button" class="btn btn-yellow btn-more">Administrar Orden</button>
+                  <button type="button" class="btn btn-yellow btn-more" data-toggle="modal" data-target="#modalInfoOrders">Administrar Orden</button>
                   <h3 class="date-order">Estado:  {{$order->status_order}}</h3>
                 </div>
               @elseif($order->status_order == 'Finalizado')
@@ -139,3 +139,64 @@
                 </li>  
             </ul>
         </nav>
+
+        <script src="{{ asset('js/jquery-3.4.1.min.js')}}"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+        <div class="modal fade" id="modalInfoOrders" tabindex="-1" role="dialog" aria-labelledby="modalInfoOrders" aria-hidden="true">
+          <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Informacion De La Orden</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="cardInfoOrder">
+                    <h5 class="title-info">Información Orden</h5>
+                  <div class="subCardInfo">
+                      <span><strong>Fecha Creacion:</strong> 13-07-2020</span>
+                  </div>
+                  <div class="subCardInfo">
+                      <span><strong>Estado:</strong> Pendiente</span>
+                  </div>
+                  <div class="subCardInfo">
+                      <span><strong>Fecha Servicio:</strong> 13-07-2020</span>
+                  </div>
+                  <div class="subCardInfo">
+                      <span><strong>Score:</strong> 1</span>
+                  </div>
+                </div>
+
+
+
+                <div class="cardInfoUser">
+                <h5 class="title-info">Información Cliente</h5>
+                  <div class="subCardInfoUser2">
+                      <span><strong>Cliente:</strong> Andres Felipe Ospina Muñoz</span>
+                  </div>
+                  <div class="subCardInfoUser2">
+                      <span><strong>Ciudad:</strong> Manizales</span>
+                  </div>
+                  <div class="subCardInfoUser2">
+                      <span><strong>Direccion:</strong> Calle 51 E # 34 - 17</span>
+                  </div>  
+                  <div class="subCardInfoUser2">
+                      <span><strong>Barrio:</strong> Santos</span>
+                  </div>
+                  <div class="subCardInfoUser2">
+                      <span><strong>Telefono:</strong> 3122660370</span>
+                  </div>
+                  <div class="subCardInfoUser2">
+                      <span><strong>Email:</strong> pipe96-17@hotmail.com</span>
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
