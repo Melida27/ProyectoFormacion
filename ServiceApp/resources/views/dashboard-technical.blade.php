5094f7537f7b685a325ae03b0b16fc6ae6069bb6
pipeosmu <pipe96-17@hotmail.com>
2020-07-15 08:47:10 -0500

    Menu general tecnico

--- ServiceApp/resources/views/dashboard-technical.blade.php
+++ ServiceApp/resources/views/dashboard-technical.blade.php
@@ -6,103 +6,7 @@
       <img class="img-home" src="{{ asset('imgs/home_technical.svg') }}">
     </div>
 
-  <nav class="main-menu">
-            <ul>
-                <li>
-                    <a href="{{ url('home') }}">
-                        <i class="fa fa-home fa-2x"></i>
-                        <span class="nav-text">
-                            Inicio
-                        </span>
-                    </a>
-                  
-
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                        <i class="fa fa-laptop fa-2x"></i>
-                        <span class="nav-text">
-                            Stars Components
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="{{ url('studies') }}">
-                       <i class="fa fa-list fa-2x"></i>
-                        <span class="nav-text">
-                           Mis Estudios
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-folder-open fa-2x"></i>
-                        <span class="nav-text">
-                            Pages
-                        </span>
-                    </a>
-                   
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-bar-chart-o fa-2x"></i>
-                        <span class="nav-text">
-                            Graphs and Statistics
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-font fa-2x"></i>
-                        <span class="nav-text">
-                           Quotes
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="{{url('orders')}}">
-                       <i class="fa fa-table fa-2x"></i>
-                        <span class="nav-text">
-                            Mis Ordenes
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                        <i class="fa fa-map-marker fa-2x"></i>
-                        <span class="nav-text">
-                            Maps
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-info fa-2x"></i>
-                        <span class="nav-text">
-                            Documentation
-                        </span>
-                    </a>
-                </li>
-            </ul>
-
-            <ul class="logout">
-                <li>
-                   <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                         <i class="fa fa-power-off fa-2x"></i>
-                        <span class="nav-text">
-                            Cerrar Sesion
-                        </span>
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                    </form>
-                </li>  
-            </ul>
-        </nav>
-        
+  
         <script src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
         <script src="{{ asset('js/sweetalert2@9.js') }}"></script>
 
@@ -132,3 +36,5 @@
                 /* ----------------------------------------------------------------------------- */
             });
         </script>
+
+        @include('users.technical.technical-menu')
