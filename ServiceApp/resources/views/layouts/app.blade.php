02b049f7d81396f06cc72d22f63d2b9d509d1244
Melida27 <mjgutierrez56@misena.edu.co>
2020-07-26 19:40:39 -0500

    Nombre proyecto - Navbar

--- ServiceApp/resources/views/layouts/app.blade.php
+++ ServiceApp/resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-dark bg-custom shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'HomeServices') }}
+                    Home Services                
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
